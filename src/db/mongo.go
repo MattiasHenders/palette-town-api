8df6ee5d0f31bc626a339c5dea45fce72762a81a
MattiasHenders <mattias@makeship.com>
2022-10-30 19:18:31 -0700

    Uneeded comments

--- src/db/mongo.go
+++ src/db/mongo.go
@@ -15,12 +15,7 @@ var Mongo *models.MongoDB
 
 func Close() {
 
-	// client provides a method to close
-	// a mongoDB connection.
 	defer func() {
-
-		// client.Disconnect method also has deadline.
-		// returns error if any,
 		if err := Mongo.Client.Disconnect(Mongo.Context); err != nil {
 			panic(err)
 		}
@@ -34,11 +29,7 @@ func Connect() {
 	completeURI := strings.ReplaceAll(dbConfig.URI, "<USERNAME>", dbConfig.Username)
 	completeURI = strings.ReplaceAll(completeURI, "<PASSWORD>", dbConfig.Password)
 
-	// ctx will be used to set deadline for process, here
-	// deadline will of 30 seconds.
 	ctx := context.Background()
-
-	// mongo.Connect return mongo.Client method
 	client, _ := mongo.Connect(ctx, options.Client().ApplyURI(completeURI))
 
 	mongoTemp := models.MongoDB{
@@ -52,10 +43,6 @@ func Connect() {
 
 func Ping() error {
 
-	// mongo.Client has Ping to ping mongoDB, deadline of
-	// the Ping method will be determined by cxt
-	// Ping method return error if any occurred, then
-	// the error can be handled.
 	if err := Mongo.Client.Ping(Mongo.Context, readpref.Primary()); err != nil {
 		return err
 	}
